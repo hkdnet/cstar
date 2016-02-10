1a2d7d7f98d622261558dfda123ad919a30ca0a1
hkdnet <hkdnet@users.noreply.github.com>
2016-02-11 01:32:05 +0900

    実行フォルダ配下の.gitディレクトリを列挙できるようにする

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/codegangsta/cli"
 )
@@ -20,8 +21,40 @@ func main() {
 	app.Commands = Commands
 	app.CommandNotFound = CommandNotFound
 	app.Action = func(c *cli.Context) {
-			fmt.Println("hello world")
+		ch := make(chan string)
+		pwd, _ := os.Getwd()
+		fmt.Println(pwd)
+		go getGitLists(pwd, ch)
+		for {
+			path, ok := <-ch
+			if !ok {
+				return
+			}
+			fmt.Println(path)
+		}
 	}
 
 	app.Run(os.Args)
+}
+
+func getGitLists(root string, ch chan string) {
+	err := filepath.Walk(root,
+		func(path string, info os.FileInfo, err error) error {
+			rel, err := filepath.Rel(root, path)
+			if err != nil {
+				close(ch)
+				return nil
+			}
+			if filepath.Base(rel) == ".git" {
+				path, _ := filepath.Abs(rel)
+				ch <- path
+				return nil
+			}
+			return nil
+		})
+
+	if err != nil {
+		fmt.Println(1, err)
+	}
+	close(ch)
 }
