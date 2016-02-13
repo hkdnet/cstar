aab845e8beae8970b37631489af15855dfdee28d
hkdnet <hkdnet@users.noreply.github.com>
2016-02-13 20:52:54 +0900

    WIP

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/codegangsta/cli"
@@ -41,7 +42,7 @@ func main() {
 
 func search(root string, ch chan string) {
 	dirCh := make(chan string)
-	logCh := make(chan string)
+	logCh := make(chan CommitCount)
 	go gitDirSearch(root, dirCh)
 	go gitDirToLog(dirCh, logCh)
 	for {
@@ -52,6 +53,20 @@ func search(root string, ch chan string) {
 		fmt.Println(log)
 	}
 	close(ch)
+}
+
+type CommitCount struct {
+	ProjectName string
+	Count       []int
+}
+type Commit struct {
+	ProjectName string
+	At          time.Time
+	Author      string
+}
+
+func (c Commit) String() string {
+	return fmt.Sprintf("%s %s %s", c.ProjectName, c.At.Format("2006-01-02"), c.Author)
 }
 
 func gitDirSearch(root string, dirCh chan string) {
@@ -73,7 +88,7 @@ func gitDirSearch(root string, dirCh chan string) {
 	}
 	close(dirCh)
 }
-func gitDirToLog(dirCh, logCh chan string) {
+func gitDirToLog(dirCh chan string, logCh chan CommitCount) {
 	for {
 		dir, ok := <-dirCh
 		if !ok {
@@ -83,14 +98,36 @@ func gitDirToLog(dirCh, logCh chan string) {
 		os.Chdir(dir + "/../")
 		pwd, _ := os.Getwd()
 		pjName := filepath.Base(pwd)
-		logCh <- fmt.Sprintf("Project: %s", pjName)
-		since := time.Now().AddDate(0, 0, -1).Format(time.RFC3339)
+		dayCount := 7
+		since := time.Now().AddDate(0, 0, -dayCount).Format(time.RFC3339)
 		out, err := exec.Command("git", "reflog", "--oneline", "--date=short", "--pretty=format:%ad %an", "--since", since).Output()
 		if err != nil {
-			logCh <- err.Error()
 			close(logCh)
 			return
 		}
-		logCh <- string(out)
+		raw_log := string(out)
+		if raw_log == "" {
+			logCh <- CommitCount{pjName, make([]int, dayCount)}
+			continue
+		}
+		counts := make([]int, dayCount)
+		/*
+			logs := strings.Split(raw_log, "\n")
+			for _, log := range logs {
+				 arr := strings.Split(log, " ")
+				 at, _ := time.Parse("2006-01-02", arr[0])
+			}
+		*/
+		logCh <- CommitCount{pjName, counts}
 	}
+}
+func logToCountPerDay(logs string) map[string]int {
+	ret := map[string]int{}
+	for _, log := range strings.Split(logs, "\n") {
+		tmp := strings.Split(log, " ")
+		// mapに存在しないときはdefault(int) -> 0
+		count := ret[tmp[0]]
+		ret[tmp[0]] = count + 1
+	}
+	return ret
 }
--- main_test.go
+++ main_test.go
@@ -7,6 +7,10 @@ import (
 
 func TestGitDirSearch(t *testing.T) {
 	pwd, _ := os.Getwd()
+	if err := beforeTestGitDirSearch(pwd); err != nil {
+		t.Fatalf("Got Error: %v", err)
+		return
+	}
 	ch := make(chan string)
 	go gitDirSearch(pwd+"/test", ch)
 	dirs := []string{}
@@ -22,12 +26,38 @@ func TestGitDirSearch(t *testing.T) {
 		for _, s := range dirs {
 			t.Errorf(s)
 		}
+		return
 	}
 	expected1 := pwd + "/test/test_dir1/.git"
 	expected2 := pwd + "/test/test_dir2/.git"
-	if (dirs[0] != expected1 && dirs[1] != expected1) ||
-		(dirs[0] != expected2 && dirs[1] != expected2) {
-		t.Errorf("length of dirs should be \n - %s\n - %s\n but actually \n - %s\n - %s", expected1, expected2, dirs[0], dirs[1])
+	if !((dirs[0] == expected1 || dirs[1] == expected1) && (dirs[0] == expected2 || dirs[1] == expected2)) && (dirs[0] != dirs[1]) {
+		t.Errorf("dirs should be \n - %s\n - %s\n but actually \n - %s\n - %s", expected1, expected2, dirs[0], dirs[1])
+	}
+	if err := afterTestGitDirSearch(pwd); err != nil {
+		t.Fatalf("Got Error: %v", err)
+	}
+}
+func beforeTestGitDirSearch(pwd string) error {
+	for _, path := range []string{"/test/test_dir1/.git", "/test/test_dir2/.git"} {
+		if err := os.MkdirAll(pwd+path, 0755); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func afterTestGitDirSearch(pwd string) error {
+	if err := os.RemoveAll(pwd + "/test"); err != nil {
+		return err
+	}
+	return nil
+}
 
+func TestLogToCountPerDay(t *testing.T) {
+	logs := "key1 hoge\nkey2 fuga^\nkey1 piyo"
+	ret := logToCountPerDay(logs)
+	if val1, ok := ret["key1"]; !ok {
+		t.Errorf("map should have key1: %v", ret)
+	} else if val1 != 2 {
+		t.Errorf("the value of key1 should be 2, but %v", val1)
 	}
 }
